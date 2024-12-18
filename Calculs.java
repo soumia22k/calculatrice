ac6b8e3279bbb9414724c406a53baeec10ac9372
soumia22k <kablaouisoumia75@gmail.com>
2024-12-19 00:24:16 +0100

    Update Calculs.java

--- Calculs.java
+++ Calculs.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Calculs {
+    public class Calculs {
     static Scanner input = new Scanner(System.in);
     static float a;
     static float b;
@@ -13,49 +13,54 @@ public class Calculs {
 
     static void addition() {
         valeurs();
-        System.out.println("la somme de" + a + "et" + b + "est: " + (a + b));}
+        System.out.println("la somme de:\n" + a + "+" + b + "\nc'est: " + (a + b));}
 
     static void soustraction() {
         valeurs();
-        System.out.println(a + "-" + b + "est: " + (a - b));
+        System.out.println("la soustraction de:\n" + a + "-" + b + "\nc'est: " + (a - b));
     }
 
     static void multiplication() {
         valeurs();
-        System.out.println(a + "multiplié par" + b + "est: " + (a * b));
+        System.out.println("la multiplication de:\n" + a + "*" + b + "\nC'est: " + (a * b));
     }
 
     static void division() {
         valeurs();
-        System.out.println(a + "divisé par" + b + "est:" + (a / b));
+        System.out.println("la division de:\n" + a + "/" + b + "\nc'est:" + (a / b));
 
     }
 
     static void puissance() {
         valeurs();
-        System.out.println("la puissance de" + a + "^" + b + "est:" + Math.pow(a, b));
+        System.out.println("la puissance de:\n" + a + "^" + b + "\nc'est:" + Math.pow(a, b));
     }
     static void racine() {
         System.out.println("entrer le numero: ");
-        System.out.println("la racine carré est :" + Math.sqrt(a));
+        a=input.nextFloat();
+        System.out.println("la racine carré de:\n" + a +  "\nc'est:" +  Math.sqrt(a));
     }
 
     static void factorielle() {
-        System.out.println("entrer le numero: ");
-        int a = input.nextInt();
+        System.out.println("entrer un nombre: ");
+        int n = input.nextInt();
+        if (n < 0){
+            System.out.println("erreur , C'est un nombre négatif  ne définie pas par la fctorielle!! ");
+            return;
+        }
         int f = 1;
-        for (int i = 1; i <= a; i++) {
+        for (int i = 1; i <= n; i++) {
             f *= i;
         }
-        System.out.println("la factorielle de" + a + "est: " + f);
+        System.out.println("la factorielle de:\n" + n + "\nc'est:" + f);
 
 
     }
     public static void main(String[] args) {
 
-        int n;
+        int ch;
         do {
-            System.out.println("MENU");
+            System.out.println("MENU:");
             System.out.println("1: ADDITION");
             System.out.println("2: SOUSTRACTION");
             System.out.println("3: MULTIPLICATION");
@@ -65,9 +70,9 @@ public class Calculs {
             System.out.println("7: FACTORIELLE");
             System.out.println("8: QUITTER");
 
-            System.out.println("ENTRER VOTRE CHOIX");
-            n = input.nextInt();
-            switch (n) {
+            System.out.println("ENTRER VOTRE CHOIX:");
+            ch = input.nextInt();
+            switch (ch) {
                 case 1:
                     addition();
                     break;
@@ -90,12 +95,12 @@ public class Calculs {
                     factorielle();
                     break;
                 case 8:
-                    System.out.println("quitter");
+                    System.out.println("Quitter");
                     break;
                 default:
-                    System.out.println("choix invalid.");
+                    System.out.println("le Choix invalide ,réessayer!!");
             }
         }
-        while (n != 8);
+        while (ch != 8);
     }
 }
